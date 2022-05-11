16a3b6a98a65087d36852b37b183c16a38d66535
winter-b <85021800+winter-b@users.noreply.github.com>
2022-05-11 04:38:20 +0300

    repeat execution

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const {Client, Intents} = require('discord.js');
 const intents = new Intents(32767);
 const token = require('./settings.json').Discord_bot_token;
+const channel_id = require('./settings.json').Discord_channel_id;
 const discordClient = new Client({intents});
-discordClient.login(token);
-discordClient.on('ready', () => {
-    console.log(`Logged in as ${discordClient.user.tag}!`);
-    var exec = require('child_process').exec;
-    exec('killall ngrok | ngrok http 80 > /dev/null &', function (error, stdout, stderr) {
+var exec = require('child_process').exec;
+
+function startExecution(){
+    executeCommand('kill ngrok');
+    executeCommand('ngrok http 80 > /dev/null &');
+    setTimeout(() => {
+        exec('curl localhost:4040/api/tunnels', function (error, stdout, stderr) {
+            console.log(stdout);
+            var url = JSON.parse(stdout).tunnels[0].public_url;
+            console.log(url)
+            discordClient.channels.cache.get(channel_id).send(url);
+        });
+    }, 1000);
+    //repeat every two hours
+    setTimeout(startExecution, 7200000);
+}
+
+function executeCommand(command) {
+    exec(command, function (error, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         if (error !== null) {
             console.log('exec error: ' + error);
         }
     });
-    exec('curl localhost:4040/api/tunnels', function (error, stdout, stderr) {
-        console.log(stdout);
-        var url = stdout.split('"public_url":"')[1].split('"')[0];
-        console.log(url)
-    });
+}
+
+discordClient.login(token);
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`);
+    startExecution();
 });
+
